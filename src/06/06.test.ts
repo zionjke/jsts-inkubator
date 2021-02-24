7724a9f093fc46ad8186ca5600d16661a6390b91
artem_vasiliev <vincere802@gmail.com>
2021-02-24 16:55:12 +0200

    lesson2 - destructuring assignment

--- src/06/06.test.ts
+++ src/06/06.test.ts
@@ -1,3 +1,23 @@
 test('some test', () => {
 
+    let man = {
+        name: 'Artem',
+        age: 32,
+        lessons: [
+            {title: '1'},
+            {title: '2'}
+        ]
+    }
+
+    const {age,lessons} = man
+
+    const a = man.age
+    const l = man.lessons
+
+    expect(age).toBe(32)
+    expect(lessons.length).toBe(2)
+
+    expect(a).toBe(32)
+    expect(l.length).toBe(2)
+
 })
