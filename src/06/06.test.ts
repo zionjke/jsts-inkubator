a992f9d5cbddf8d17c6d0ec55da709faad155b10
zionjke <vincere802@gmail.com>
2021-02-24 20:43:07 +0200

    lesson2 - destructuring assignment ++

--- src/06/06.test.ts
+++ src/06/06.test.ts
@@ -1,23 +1,75 @@
-test('some test', () => {
+type ManType = {
+    name: string
+    age: number
+    lessons: Array<LessonType>
+    address: AddressType
+}
 
-    let man = {
+type LessonType = {
+    title: string
+}
+
+type AddressType = {
+    street: StreetType
+}
+
+type StreetType = {
+    title: string
+}
+
+let man: ManType
+
+beforeEach(() => {
+
+    man = {
         name: 'Artem',
         age: 32,
         lessons: [
             {title: '1'},
-            {title: '2'}
-        ]
+            {title: '2'},
+            {title: '3'},
+        ],
+        address: {
+            street: {
+                title: 'Bulgakova street'
+            }
+        }
     }
 
-    const {age,lessons} = man
+})
+
+test('some test', () => {
+
+
+    const {age, lessons} = man
 
     const a = man.age
     const l = man.lessons
 
     expect(age).toBe(32)
-    expect(lessons.length).toBe(2)
+    expect(lessons.length).toBe(3)
 
     expect(a).toBe(32)
-    expect(l.length).toBe(2)
+    expect(l.length).toBe(3)
 
+})
+
+
+test('', () => {
+
+    const ls1 = man.lessons[0]
+    const ls2 = man.lessons[1]
+
+    const [l1, l2] = man.lessons
+    const [,les2,...rest] = man.lessons
+
+    expect(l1.title).toBe('1')
+    expect(l2.title).toBe('2')
+
+    expect(ls1.title).toBe('1')
+    expect(l2.title).toBe('2')
+
+    expect(les2.title).toBe('2')
+    expect(rest.length).toBe(1)
+    expect(rest[0]).toStrictEqual({title: '3'})
 })
